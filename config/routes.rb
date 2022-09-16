6c16f8f97271210dccf3744a4372b25965f3582e
Gauri Joshi <gauri728@gmail.com>
2022-09-16 12:45:56 -0700

    route resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   #maybe redirect later to different controller action
   get '/auth/google_oauth2/callback', to: 'users#create'
-  get '/thrones/new', to: 'thrones#new'
+  # get '/thrones/new', to: 'thrones#new'
+  resources :thrones, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
