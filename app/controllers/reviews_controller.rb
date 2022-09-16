35c8f0ab546ce46b75f8d044270d1327642eaf71
Ben Silverstein <benjsilverstein@comcast.net>
2022-09-16 14:42:06 -0600

    Add review form

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,7 @@
+class ReviewsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get '/', to: "welcome#index"
-  
+
   #maybe redirect later to different controller action
   get '/auth/google_oauth2/callback', to: 'users#create'
+  get '/reviews/new', to: 'reviews#new'
   # get '/thrones/new', to: 'thrones#new'
   resources :thrones, only: [:new]
   delete 'logout', to: 'users#destroy'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
