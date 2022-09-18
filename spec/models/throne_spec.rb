b707dda1fa8eb228b5064aa98e36f1f5c5b68cd9
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-18 12:00:00 -0600

    add test for throne poro

--- spec/models/throne_spec.rb
+++
@@ -1,8 +0,0 @@
-# require 'rails_helper'
-
-# RSpec.describe Throne, type: :model do 
-#   describe 'validations' do 
-#     it { should allow_value(true). for(:baby_changing)}
-#     it { should allow_value(false). for(:baby_changing)}
-#   end
-# end
