8a54bacc22b99b09d180451556a32713a46ab4b8
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-19 15:38:29 -0600

    something something the web

--- app/controllers/thrones_controller.rb
+++ app/controllers/thrones_controller.rb
@@ -1,11 +1,15 @@
+require "uri"
+require "net/http"
+
 class ThronesController < ApplicationController 
   def new
     
   end
 
-  def create
-    require 'pry'; binding.pry
-    #location: params[:location], direction: params[:direction]
+  def create 
+    not_params = {"throne_room" => {name: params[:name], location: params[:location], direction: params[:direction], baby_changing_station: params[:baby_changing_station], bathroom_style: params[:bathroom_style], key_code_required: params[:key_code_required]}}
+    send = Net::HTTP.post_form(URI.parse('https://the-throne-room-api.herokuapp.com/api/v1/throne_rooms'), not_params)
+    puts send.body
     redirect_to '/'
   end
 
