1b306ce1049e359bf6c3466047385e3d4e2c8e1e
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-21 11:08:13 -0600

    adjusted test

--- spec/services/review_service_spec.rb
+++ spec/services/review_service_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ReviewService do 
   it 'gets data from api' do 
-    reviews = ReviewService.get_reviews
+    reviews = ReviewService.get_reviews(1)
     expect(reviews).to be_a(Array)
-
+    
     review = reviews[0]
     #we don't have reviews data saved yet so there is
     #no review data to 'get' from the BE api
