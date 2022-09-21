c30b4d2a522c1112f44eba4eb31d16fde90e762a
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-21 10:17:00 -0600

    changed routes in servies for  reviews

--- app/facades/review_facade.rb
+++ app/facades/review_facade.rb
@@ -5,12 +5,6 @@ class ReviewFacade
             Review.new(review)
         end
     end
-    
-###creates a new review
-    def self.new_review(user_id)
-      review = ReviewService.create_review(user_id)
-      Review.new(review)
-    end
 
     def self.send(params)
       review = ReviewService.send_review(params)
--- app/services/review_service.rb
+++ app/services/review_service.rb
@@ -4,18 +4,14 @@ class ReviewService
         Faraday.new(url: "https://the-throne-room-api.herokuapp.com")
     end
 
-    def self.get_reviews
-        response = conn.get("/api/v1/reviews")
-        JSON.parse(response.body, symbolize_names: true)[:data]
-    end
-    
-    def self.create_review
-        response = conn.post("/api/v1/reviews")
+    def self.get_reviews(id)
+        response = conn.get("api/v1/throne_rooms/#{id}/reviews")
         JSON.parse(response.body, symbolize_names: true)[:data]
     end
 
     def self.send_review(params)
-        response = conn.post("/api/v1/reviews") do |thing|
+        require 'pry'; binding.pry 
+        response = conn.post("api/v1/throne_rooms/#{params["reviews"][:throne_room_id]}/reviews") do |thing|
             thing.params = params
         end
     end
