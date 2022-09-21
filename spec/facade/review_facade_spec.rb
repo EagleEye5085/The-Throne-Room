cdff5a4779441f90895da3cde47d4427cacf4066
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-21 15:01:55 -0600

    fixed tests for review

--- spec/facade/review_facade_spec.rb
+++ spec/facade/review_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ReviewFacade do 
   it 'reads data from api' do 
-    reviews = ReviewFacade.all_reviews
+    reviews = ReviewFacade.all_reviews(2)
 
     expect(reviews).to be_a(Array)
     expect(reviews).to be_all(Review)
--- spec/services/review_service_spec.rb
+++ spec/services/review_service_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe ReviewService do 
   it 'gets data from api' do 
-    reviews = ReviewService.get_reviews(1)
+    reviews = ReviewService.get_reviews(2)
     expect(reviews).to be_a(Array)
-    
-    review = reviews[0]
-    #we don't have reviews data saved yet so there is
-    #no review data to 'get' from the BE api
+
+    review = reviews[0][:attributes]
+        
+        expect(review[:cleanliness]).to be_a(Integer)
+        expect(review[:ambiance]).to be_a(Integer)
+        expect(review[:tp_quality]).to be_a(Integer)
+        expect(review[:privacy]).to be_a(Integer)
+        expect(review[:other_comments]).to be_a(String)
+        expect(review[:throne_room_id]).to be_a(Integer)
+        expect(review[:user_id]).to be_a(Integer)
   end
 end
